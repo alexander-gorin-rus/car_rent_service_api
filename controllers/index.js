7b2c0b90632ff002158b1575e03061b56ec115cf
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-03-14 00:03:39 +0600

    3

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const db = require("../db")
 
 exports.getCars = async (req, res) => {
-    const result = await db.query('SELECT * from cars WHERE car_type = \'vagon\'')
+    const result = await db.query('SELECT * FROM car_rent_report WHERE end_rent < now()')
     //console.log(result[rows]);
     try {
         res.status(200).json({
