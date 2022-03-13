3268141499f800b7a5e7eccbb6d40c44ccbe501d
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-03-13 20:34:26 +0600

    file db/db.sql was added to .gitignore

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,16 @@
 const db = require("../db")
 
 exports.getCars = async (req, res) => {
-    const result = await db.query('SELECT * from cars WHERE available = true')
-    console.log(result);
+    const result = await db.query('SELECT * from cars WHERE car_type = \'vagon\'')
+    //console.log(result[rows]);
     try {
-         res.status(200).json(result)
+        res.status(200).json({
+            status: "success",
+            results: result.rows.length,
+            data: {
+                cars: result.rows
+            } 
+        })
     } catch (error) {
         console.log(error)
     }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use('/api/v1', require('./routes'));
