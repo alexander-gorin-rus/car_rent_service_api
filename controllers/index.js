1cb2b5ab2c0e77ce8be2bedf282fbd1cc8b48305
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-03-14 23:03:16 +0600

    joined two tables

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,13 @@
 const db = require("../db")
 
 exports.getCars = async (req, res) => {
-    const result = await db.query('SELECT * FROM car_rent_report WHERE end_rent < now()')
+    const result = await db.query('SELECT * FROM car_rent_report JOIN cars ON car_rent_report.car_id = cars.car_id WHERE end_rent < now();')
     //console.log(result[rows]);
     try {
         res.status(200).json({
-            status: "success",
-            results: result.rows.length,
-            data: {
-                cars: result.rows
-            } 
+            // status: "success",
+            // results: result.rows.length,
+            cars: result.rows 
         })
     } catch (error) {
         console.log(error)
